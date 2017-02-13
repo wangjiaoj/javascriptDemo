f3268acd00971a7ab4a451bb73c7d20daaaa62d1
wangjiaoj <2386467485@qq.com>
2017-02-13 19:55:35 +0800

    change

--- myNodeProject/www/MyDatePicker/js/datepicker.js
+++ myNodeProject/www/MyDatePicker/js/datepicker.js
@@ -4,6 +4,7 @@
 
      var defaultOptions = {
          el: 'id', //日历显示定位位置的id，当选中日期后将数据显示到该id内
+         date: new Date(), //默认选中当前日期
          minDate: "1900-01-01 00:00:00",
          maxDate: "2099-12-31 23:59:59",
          eCont: 'id', //单纯当做日历使用的元素id
@@ -16,7 +17,8 @@
          onpicked: function() {}, //选中日期时的回调函数
          calendars: 1, //单日历还是双日历
          current: '', //2017-01-01
-         onRenderCell: function() { return {} }
+         onRenderCell: function() { return {} },
+         onBeforeShow: function() {}
      }
      var ids = {},
          views = {
@@ -25,7 +27,6 @@
              days: 'datepickerViewDays'
          },
          tpl = {
-
              header: [
                  '<div class="dpTitle">',
                  '<div class="navImg datepickerYearGoPrev"><a></a></div><div class="navImg datepickerMonthGoPrev"><a></a></div>',
@@ -118,15 +119,15 @@
                  '</tbody></table>'
              ],
              months: [
-                 '<table cellspacing="0" cellpadding="3" border="0" nowrap="nowrap"><tbody>',
-                 '<tr><td nowrap="" class="menu"><%=data[0]%></td><td nowrap="" class="menu"><%=data[6]%></td></tr>',
-                 '<tr nowrap="nowrap"><td nowrap="" class="menu" ><%=data[1]%></td><td nowrap="" class="menu"><%=data[7]%></td></tr>',
-                 '<tr nowrap="nowrap"><td nowrap="" class="menu"><%=data[2]%></td><td nowrap="" class="menu"><%=data[8]%></td></tr>',
-                 '<tr nowrap="nowrap"><td nowrap="" class="menu"><%=data[3]%></td><td nowrap="" class="menu"><%=data[9]%> </td></tr>',
-                 '<tr nowrap="nowrap"><td nowrap="" class="menu"><%=data[4]%></td><td nowrap="" class="menu"><%=data[10]%></td></tr>',
-                 '<tr nowrap="nowrap "><td nowrap="" class="menu"><%=data[5]%></td><td nowrap="" class="menu"><%=data[11]%></td></tr>',
+                 '<table cellspacing="0" cellpadding="3" border="0" nowrap="nowrap" data-year="<%=currentYear%>"><tbody>',
+                 '<tr><td                  class="<%=data[0].classname%>"><%=data[0].text%></td><td  class="<%=data[6].classname%>"><%=data[6].text%></td></tr>',
+                 '<tr nowrap="nowrap"><td  class="<%=data[1].classname%>"><%=data[1].text%></td><td  class="<%=data[7].classname%>"><%=data[7].text%></td></tr>',
+                 '<tr nowrap="nowrap"><td  class="<%=data[2].classname%>"><%=data[2].text%></td><td  class="<%=data[7].classname%>"><%=data[8].text%></td></tr>',
+                 '<tr nowrap="nowrap"><td  class="<%=data[3].classname%>"><%=data[3].text%></td><td  class="<%=data[9].classname%>"><%=data[9].text%> </td></tr>',
+                 '<tr nowrap="nowrap"><td  class="<%=data[4].classname%>"><%=data[4].text%></td><td  class="<%=data[10].classname%>"><%=data[10].text%></td></tr>',
+                 '<tr nowrap="nowrap"><td  class="<%=data[5].classname%>"><%=data[5].text%></td><td  class="<%=data[11].classname%>"><%=data[11].text%></td></tr>',
                  '</tbody></table>',
-                 '<table cellspacing = "0" cellpadding = "3" border = "0" align = "center"> <tbody> <tr> <td class = "yearMenuGoprev yearMenuControl"> ← </td><td class="yearMenuClose yearMenuControl">×</td> <td class = "yearMenuGoNext yearMenuControl"> → </td></tr> </tbody></table > '
+                 '<table cellspacing = "0" cellpadding = "3" border = "0" align = "center"> <tbody> <tr> <td class = "<%=prevClassName%>"> ← </td><td class="yearMenuClose yearMenuControl">×</td> <td class = "<%=nextClassName%>"> → </td></tr> </tbody></table > '
              ]
 
          };
@@ -138,9 +139,10 @@
      var DatePicker = function(options) {
          this.options = $.extend({}, defaultOptions, options || {});
          this.init();
-
      }; // DatePicker
-     var fn = DatePicker.prototype
+
+     var fn = DatePicker.prototype;
+
      fn.init = function() {
          //设置当前时间,如果未设定开始时间,则取当前日期为默认值
          if (this.options.startDate) {
@@ -150,12 +152,11 @@
              this.options.current = current.getFullYear() + "-" + (current.getMonth() + 1) + "-" + current.getDate();
          }
          this.bulidCalender();
-
      }
      fn.bulidCalender = function() {
-         var html = this.fill();
-         html = '<div  class="datepickerDays">' + html + '</div>';
-         var $WdateDiv = $('<div class="WdateDiv"></div>');
+         var html = this.bulidDay();
+         html = '<div  class="datepickerDays" >' + html + '</div>';
+         var $WdateDiv = $('<div class="WdateDiv" style="visibility: hidden;"></div>');
          $WdateDiv.append(tpl.header.join(""), html, tpl.time.join(""), tpl.Quickselect, tpl.control.join(""));
          var dataPicker = $("body").append($WdateDiv);
          var date = new Date(this.options.current);
@@ -163,11 +164,12 @@
          $WdateDiv.find(".datapickerinput").eq(1).val(date.getFullYear());
          this.wrapper = $WdateDiv;
          this.bindHeader();
-         this.bindDay()
-             //this.bindControl();
+         this.bindDay();
+         this.bindControl();
+
      }
 
-     fn.fill = function() {
+     fn.bulidDay = function() {
          var options = this.options;
          var cnt, date;
 
@@ -216,7 +218,10 @@
                  var fromUser = options.onRenderCell(date);
                  var val = date.valueOf();
                  if (options.date && (!$.isArray(options.date) || options.date.length > 0)) {
-                     if (fromUser.selected || options.date == val || $.inArray(val, options.date) > -1 || (options.mode == 'range' && val >= options.date[0] && val <= options.date[1])) {
+                     //  if (fromUser.selected || options.date == val || $.inArray(val, options.date) > -1 || (options.mode == 'range' && val >= options.date[0] && val <= options.date[1])) {
+                     //      data.weeks[indic].days[indic2].classname.push('datepickerSelected');
+                     //  }
+                     if (options.date.getDate() == date.getDate() && options.date.getMonth() == date.getMonth() && options.date.getYear() == date.getYear()) {
                          data.weeks[indic].days[indic2].classname.push('datepickerSelected');
                      }
                  }
@@ -237,16 +242,42 @@
      }
 
      fn.bulidYearMenu = function(year) {
-         var html, data, dow;
+         var maxYear = new Date(this.options.maxDate).getFullYear();
+         var minYear = new Date(this.options.minDate).getFullYear();
+         var html, data, dow, year;
 
          dow = year - 6;
          data = {
              data: [],
-             className: 'datepickerYears'
+             currentYear: year,
+             prevClassName: "",
+             nextClassname: ""
+         }
+         if (minYear >= dow) {
+             data.prevClassName = "invalidMenu";
+         } else {
+             data.prevClassName = "yearMenuGoprev yearMenuControl";
          }
          for (var j = 0; j < 12; j++) {
-             data.data.push(dow + j);
+             year = dow + j;
+             data.data[j] = {
+                 text: year,
+                 classname: []
+             };
+
+             if (maxYear >= year) {
+                 data.data[j].classname.push("menu");
+             } else {
+                 data.data[j].classname.push("invalidMenu");
+             }
          }
+         if (maxYear >= year) {
+             data.nextClassName = "yearMenuGoNext yearMenuControl";
+         } else {
+             data.nextClassName = "invalidMenu";
+
+         }
+
          // datepickerYears template
          html = tmpl(tpl.months.join(''), data);
          return html;
@@ -282,21 +313,23 @@
              } else {
 
                  e.stopPropagation();
-
                  if (ele.hasClass("yearMenuGoprev")) {
                      var table = $(this).parents("table").siblings("table");
-                     table.find("td").each(function() {
-                         var year = parseInt($(this).text()) - 12;
-                         $(this).text(year);
-                     });
+                     if (table.length) {
+                         var year = parseInt(table.attr("data-year")) - 12;
+                     }
+                     var html = self.bulidYearMenu(year);
+                     table.parent(".YMenu").empty().append(html);
+
                  } else if (ele.hasClass("yearMenuClose")) {
                      var table = $(this).parents(".YMenu").css("display", "none");
                  } else if (ele.hasClass("yearMenuGoNext")) {
                      var table = $(this).parents("table").siblings("table");
-                     table.find("td").each(function() {
-                         var year = parseInt($(this).text()) + 12;
-                         $(this).text(year);
-                     });
+                     if (table.length) {
+                         var year = parseInt(table.attr("data-year")) + 12;
+                         var html = self.bulidYearMenu(year);
+                         table.parent(".YMenu").empty().append(html);
+                     }
                  }
 
              }
@@ -322,15 +355,23 @@
              }
          });
      }
+
      fn.bindDay = function() {
          var options = this.options;
          var datePickerWrapper = this.wrapper;
          var dayWrapper = datePickerWrapper.find(".datepickerDays");
-         dayWrapper.on("click", $.proxy(this.datepicker, this));
+         dayWrapper.on("click", "td", $.proxy(this.dateselect, this));
      }
-     fn.datepicker = function(e) {
-         var ele = $(e.target);
-         var day = parseInt(ele.find("span").text());
+
+     fn.dateselect = function(e) {
+         var ele = $(e.target),
+             day;
+         if (ele.is("span")) {
+             day = parseInt(ele.text());
+             ele = ele.parents("td");
+         } else {
+             day = parseInt(ele.find("span").text());
+         }
          if (ele.hasClass("datepickerInMonth")) {
              this.dateChange(5, day);
          } else if (ele.hasClass("datepickerFuture")) {
@@ -338,8 +379,12 @@
          } else {
              this.dateChange(7, day);
          }
+         // ele.addClass("datepickerSelected");
+         this.options.onpicked.call(this);
+         $(this.options.el).val(this.options.current);
          console.log(this.options.current)
      }
+
      fn.dateChange = function(YearOrMonth, num) {
          var options = this.options;
          var datePickerWrapper = this.wrapper;
@@ -369,36 +414,120 @@
                  date.addMonths(-1);
                  date.setDate(num);
          }
+         this.options.date = date;
          this.options.current = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
          //重新渲染tpl.day中的内容，并进行填充；  
-         var html = this.fill();
+         var html = this.bulidDay();
          datePickerWrapper.find(".datepickerDays").empty().html(html);
          input.eq(0).val((date.getMonth() + 1) + "月");
          input.eq(1).val(date.getFullYear());
      }
 
-     fn.controlBind = function() {
-         var options = this.options,
-             datePickerWrapper = this.wrapper;
+     fn.bindControl = function() {
+         var datePickerWrapper = this.wrapper;
          var self = this;
-         var dpControl = datePickerWrapper.find("dpControl");
+         var dpControl = datePickerWrapper.find(".dpControl");
+         var input = datePickerWrapper.find(".datapickerinput");
          dpControl.on("click", ".dpButton", function(e) {
              var ele = $(e.target);
              if (ele.hasClass("datepickerClearInput")) {
-
+                 $(self.options.el).val();
              } else if (ele.hasClass("datepickerToday")) {
-                 self.dateselect();
+                 var date = new Date();
+                 self.options.date = date;
+                 self.options.current = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+                 //重新渲染tpl.day中的内容，并进行填充；  
+                 var html = self.bulidDay();
+                 datePickerWrapper.find(".datepickerDays").empty().html(html);
+                 input.eq(0).val((date.getMonth() + 1) + "月");
+                 input.eq(1).val(date.getFullYear());
+                 $(self.options.el).val(self.options.current);
              } else if (ele.hasClass("datepickerOk")) {
-
+                 datePickerWrapper.css("visibility", 'hidden');
              }
              //datapicker消失
+         });
+         $(this.options.el).on("focus", function() {
+             self.show();
          })
+
      }
-     fn.dateselect = function() {
+
+     fn.show = function(ev) {
          var options = this.options;
-         options.onpicked.call(this);
+         var self = this;
+         var cal = this.wrapper;
+         if (!(cal.css("visibility") === "visible")) {
+             var calEl = cal.get(0);
+             //  var options = cal.data('datepicker');
 
+             //  var test = options.onBeforeShow.apply(this, [calEl]);
+             //  if (options.onBeforeShow.apply(this, [calEl]) == false) {
+             //      return;
+             //  }
 
+             //  fill(calEl);
+             //var pos = $(this).offset();
+             var pos = $(options.el).offset();
+             //  var viewPort = getViewport();
+             var top = pos.top;
+             var left = pos.left;
+
+             cal.css({
+                 visibility: 'hidden',
+                 display: 'block'
+             });
+             //  layout(calEl);
+             switch (options.position) {
+                 case 'top':
+                     top -= calEl.offsetHeight;
+                     break;
+                 case 'left':
+                     left -= calEl.offsetWidth;
+                     break;
+                 case 'right':
+                     left += this.offsetWidth;
+                     break;
+                 case 'bottom':
+                     top += this.offsetHeight;
+                     break;
+             }
+             //  if (top + calEl.offsetHeight > viewPort.t + viewPort.h) {
+             //      top = pos.top - calEl.offsetHeight;
+             //  }
+             //  if (top < viewPort.t) {
+             //      top = pos.top + this.offsetHeight + calEl.offsetHeight;
+             //  }
+             //  if (left + calEl.offsetWidth > viewPort.l + viewPort.w) {
+             //      left = pos.left - calEl.offsetWidth;
+             //  }
+             //  if (left < viewPort.l) {
+             //      left = pos.left + this.offsetWidth
+             //  }
+             cal.css({
+                 visibility: 'visible',
+                 display: 'block',
+                 top: top + 'px',
+                 left: left + 'px'
+             });
+             //  options.onAfterShow.apply(this, [cal.get(0)]);
+             $(document).on("mousedown", $.proxy(this.hidden, this));
+             // $(document).bind('mousedown', function() {
+             //     self.hidden();
+             // }); // global listener so clicking outside the calendar will close it
+         }
+         return false;
+     }
+
+     fn.hidden = function(ev) {
+         if ($(ev.target).parents(".WdateDiv").length === 0) {
+             var cal = this.wrapper;
+             cal.css({
+                 visibility: 'hidden',
+                 display: 'block'
+             });
+             $(document).off('mousedown');
+         }
      }
 
      function extendDate() {
@@ -440,36 +569,35 @@
              return d - 1;
          };
      }
-     var
-         tmpl = function tmpl(str, data) {
-             // Figure out if we're getting a template, or if we need to
-             // load the template - and be sure to cache the result.
-             var fn = !/\W/.test(str) ?
-                 cache[str] = cache[str] ||
-                 tmpl(document.getElementById(str).innerHTML) :
+     var tmpl = function tmpl(str, data) {
+         // Figure out if we're getting a template, or if we need to
+         // load the template - and be sure to cache the result.
+         var fn = !/\W/.test(str) ?
+             cache[str] = cache[str] ||
+             tmpl(document.getElementById(str).innerHTML) :
 
-                 // Generate a reusable function that will serve as a template
-                 // generator (and which will be cached).
-                 new Function("obj",
-                     "var p=[],print=function(){p.push.apply(p,arguments);};" +
+             // Generate a reusable function that will serve as a template
+             // generator (and which will be cached).
+             new Function("obj",
+                 "var p=[],print=function(){p.push.apply(p,arguments);};" +
 
-                     // Introduce the data as local variables using with(){}
-                     "with(obj){p.push('" +
+                 // Introduce the data as local variables using with(){}
+                 "with(obj){p.push('" +
 
-                     // Convert the template into pure JavaScript
-                     str
-                     .replace(/[\r\t\n]/g, " ")
-                     .split("<%").join("\t")
-                     .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-                     .replace(/\t=(.*?)%>/g, "',$1,'")
-                     .split("\t").join("');")
-                     .split("%>").join("p.push('")
-                     .split("\r").join("\\'") +
-                     "');}return p.join('');");
+                 // Convert the template into pure JavaScript
+                 str
+                 .replace(/[\r\t\n]/g, " ")
+                 .split("<%").join("\t")
+                 .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+                 .replace(/\t=(.*?)%>/g, "',$1,'")
+                 .split("\t").join("');")
+                 .split("%>").join("p.push('")
+                 .split("\r").join("\\'") +
+                 "');}return p.join('');");
 
-             // Provide some basic currying to the user
-             return data ? fn(data) : fn;
-         };
+         // Provide some basic currying to the user
+         return data ? fn(data) : fn;
+     };
 
 
      window.DatePicker = DatePicker;
