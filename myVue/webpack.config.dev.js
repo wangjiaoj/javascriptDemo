433efb80c2b52b26df8d853be70f73ed944027be
wangjiaoj <2386467485@qq.com>
2019-04-19 17:33:09 +0800

    vue demo

---
+++ myVue/webpack.config.dev.js
@@ -0,0 +1,15 @@
+const devConfig = require("./webpack.config.js");
+const devMode = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const path = require('path');
+devConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+);
+devConfig.devtool = 'source-map';
+devConfig.devServer = {
+    contentBase: path.resolve(__dirname, './dist/'),
+    compress: true,
+    hot: true, // 开启配置
+    port: 9000
+};
+module.exports = devConfig;
