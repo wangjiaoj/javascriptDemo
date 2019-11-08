7b7a2d0ed1b40308b1364f4202f51c0b29a874b8
wangjiaoj <2386467485@qq.com>
2019-11-08 10:52:47 +0800

    demo 调整

--- mocksever-master/mockroute/routers.js
+++ mocksever-master/mockroute/routers.js
@@ -1,20 +1,25 @@
 var koa = require('koa')
 var router = require('koa-router')()
 var mockdata = require('../jsonData/mockdata')
+var mockschemadata = require('../jsonData/mockschema')
 var schemadata = require('./mock')
-
-/** 
- * 路由配置 
- * API路由列表
- * 
- * 
- */
+var citydata = require('../jsonData/city')
+    /** 
+     * 路由配置 
+     * API路由列表
+     * 
+     * 
+     */
 router
     .get('/', async function(ctx, next) {
         ctx.body = mockdata
     })
     .get("/mock", async function(ctx, next) {
         ctx.body = mockdata
+    }).get("/mockschemadata", async function(ctx, next) {
+        ctx.body = mockschemadata
+    }).get("/city", async function(ctx, next) {
+        ctx.body = citydata
     });
 
 module.exports = router
